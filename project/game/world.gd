5d5770740f6f6a4ff6dbe9eaa3a597411117891f
lewhfree <48800753+lewhfree@users.noreply.github.com>
2026-01-07 21:14:49 -0700

    Better FPS, moving plane

--- project/game/world.gd
+++ project/game/world.gd
@@ -2,8 +2,13 @@ extends Node
 
 @onready var JSBSim: Node = $JSBSim
 @onready var LabelNode: Label = $Label
+@onready var Airplane: Node3D = $Airplane
+@onready var fps: Label = $fps
+@onready var fps2: Label = $fps2
 
 func _ready() -> void:
+	PhysicsServer3D.set_active(false)
+	PhysicsServer2D.set_active(false)
 	JSBSimUtils.install_jsbsim_files_to_user_dir()
 	#must be setup for relative paths
 	JSBSim.SetRootDir(ProjectSettings.globalize_path("user://JSBSim_files/"))
@@ -22,27 +27,33 @@ func _ready() -> void:
 	
 	#Get sim setup after loaded init conditions
 	JSBSim.RunIC()
+	JSBSim.SetPropertyValue("fcs/throttle-cmd-norm", 1.0)
+	JSBSim.SetPropertyValue("fcs/mixture-cmd-norm", 1.0)
+	JSBSim.SetPropertyValue("propulsion/magneto_cmd", 3)
+	JSBSim.SetPropertyValue("propulsion/starter_cmd", 1)
 
 func _physics_process(_delta: float) -> void:
-	#keep this function as minimal as possible. It should be able to run at stable 120hz
+	fps2.text = "physics FPS: " + str(1.0 / _delta)
 	JSBSim.Run()
 
 func _process(_delta: float):
-	#simple hud
-	var rot = $Airplane.rotation
-	rot = Vector3(JSBSim.GetPropertyValue("attitude/pitch-rad"), JSBSim.GetPropertyValue("attitude/heading-true-rad"), JSBSim.GetPropertyValue("attitude/roll-rad"))
-	$Airplane.rotation = rot
+	var lat = JSBSim.GetPropertyValue("position/lat-geod-rad")
+	var long = JSBSim.GetPropertyValue("position/long-gc-rad")
+	var alt = JSBSim.GetPropertyValue("position/geod-alt-ft") * 0.3048
 	
-	var pos = $Airplane.position
-	pos = Vector3(pos.x, JSBSim.GetPropertyValue("position/geod-alt-ft"), pos.x)
-	$Airplane.position = pos
-	LabelNode.text = "Lat (deg): " + str(JSBSim.GetPropertyValue("position/lat-gc-deg"))
+	var rot = Airplane.rotation
+	rot = Vector3(JSBSim.GetPropertyValue("attitude/pitch-rad"), -1 * JSBSim.GetPropertyValue("attitude/heading-true-rad"), JSBSim.GetPropertyValue("attitude/roll-rad"))
+	Airplane.rotation = rot
+	
+	var pos = Vector3((long - 2.12930168703915) * 4337000, alt, (lat - 0.82365420189374) * -6378137)
+	Airplane.position = pos
+	
+	fps.text = "FPS: " + str(1.0 / _delta)
+	
+	LabelNode.text = "Lat (rad): " + str(JSBSim.GetPropertyValue("position/lat-geod-rad"))
 	LabelNode.text += "\n"
-	LabelNode.text += "Long (deg): " + str(JSBSim.GetPropertyValue("position/long-gc-deg"))
-	#position/lat-gc-deg (RW)
-	#position/long-gc-deg (RW)
-
-	LabelNode.text += "\nHeight above sea level: " + str(JSBSim.GetPropertyValue("position/h-sl-ft"))
+	LabelNode.text += "Long (rad): " + str(JSBSim.GetPropertyValue("position/long-gc-rad"))
+	LabelNode.text += "\nHeight above sea level (m): " + str(JSBSim.GetPropertyValue("position/h-sl-ft") * 0.3048)
 	LabelNode.text += "\nPitch in deg " + str(57.2958 * JSBSim.GetPropertyValue("attitude/pitch-rad"))
 	LabelNode.text += "\nCalibrated Airspeed knots: " + str(JSBSim.GetPropertyValue("velocities/vc-kts"))
 	LabelNode.text += "\nPilot GForce(?): " + str(JSBSim.GetPropertyValue("accelerations/n-pilot-z-norm"))
